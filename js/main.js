4d83ced5bf4b04d3a5281c5b26875b8fd28c9fd3
codonculous <feidameng01@gmail.com>
2017-04-29 19:42:18 -0400

    make the model window fadein when the signin is pressed

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+
+  $('.signin').on('mousedown',function() {
+    $('.modal').fadeIn();
+
+  });
+
+});
+
+// When the user presses the .signin button, fade in the modal window
+// When the user presses the .close button, fade out the modal window
+// When the user presses the .submit button, add an .error class to both input elements
+// When the user puts their cursor in one of the input fields, remove the .error class
+// (Bonus) Allow the user to click the .modal to close itself
+// Tricky, consider what happens when two elements that are overlapping are both waiting for a click
